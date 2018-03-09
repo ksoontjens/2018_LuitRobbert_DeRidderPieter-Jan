99aa906b2eef7a02b94a45b1f5283f91978de779
Robbert <robbertluit@hotmail.com>
2018-03-09 14:26:50 +0100

    update basics & layout hoogte/breedte

--- Blu-ray_Disc_Java_Project/src/hellotvxlet/HelloTVXlet.java
+++ Blu-ray_Disc_Java_Project/src/hellotvxlet/HelloTVXlet.java
@@ -1,18 +1,21 @@
 package hellotvxlet;
 
-import javax.tv.xlet.*;
+import javax.tv.xlet.*; // Xlet
 
 // AWT
-import java.awt.event.*;
+import java.awt.event.*; // ActionListener
 import java.awt.*;
-import java.awt.Color;
+import java.awt.Color; // variabelen topLeft, topRight, bottomLeft, bottomRight 
 import java.awt.event.ActionEvent;
 import java.awt.Graphics;
+
+//  
+import javax.swing.JFrame; // variabele frame
 
 // UTILS
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
+import java.util.Random; // variabele random
 import java.util.Timer;
 
 // DVB
@@ -29,32 +32,38 @@ import org.havi.ui.HTextButton;
 import org.havi.ui.HVisible;
 import org.havi.ui.event.HActionListener;
 
-import sun.net.www.content.text.plain;
+public class HelloTVXlet implements Xlet, ActionListener {
+     
+    // Hoogte & breedte waarin het spel gaat runnen
+    public static int WIDTH     = 600;        
+    public static int HEIGHT    = 600;   
 
-public class HelloTVXlet implements Xlet {
-  
-    // Core dingen
+    // Kleuren die bij Simon Says horen
+    private Color topLeft       = Color.GREEN;
+    private Color topRight      = Color.RED;
+    private Color bottomLeft    = Color.YELLOW;
+    private Color bottomRight   = Color.BLUE;
+
+    // Essentiele dingen
     private boolean startGame;
     private boolean endGame;
     private int score;
 
     // 
     private Random random;
-
-
-    /** Panel colors. */
-    private Color linksBoven = Color.GREEN;
-    private Color topRightColor = Color.RED;
-    private Color bottomLeftColor = Color.YELLOW;
-    private Color bottomRightColor = Color.BLUE;
-
+    private JFrame frame;
+    
     public HelloTVXlet() {
-        
+        // Start het spel
+        startGame = true;
+        // Score op 0 bij start van het spel
+        score = 0;
+        // Nieuwe random bij begin van het spel
+        random = new Random();
     }
 
     public void initXlet(XletContext context) {
       
-     
     }
 
     public void startXlet() {
