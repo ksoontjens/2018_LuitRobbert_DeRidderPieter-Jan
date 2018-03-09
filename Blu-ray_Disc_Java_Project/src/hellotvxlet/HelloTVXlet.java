6af596583b3068396d18e3fa19bd6d39a9ca9b4f
Robbert <robbertluit@hotmail.com>
2018-03-09 13:44:55 +0100

    add common used imports

--- Blu-ray_Disc_Java_Project/src/hellotvxlet/HelloTVXlet.java
+++ Blu-ray_Disc_Java_Project/src/hellotvxlet/HelloTVXlet.java
@@ -2,6 +2,32 @@ package hellotvxlet;
 
 import javax.tv.xlet.*;
 
+// AWT
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.Graphics;
+
+// UTILS
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Timer;
+
+// DVB
+import org.dvb.ui.*;
+import org.dvb.event.*;
+
+// UI
+import org.havi.ui.event.*;
+import org.havi.ui.HScene;
+import org.havi.ui.HSceneFactory;
+import org.havi.ui.HState;
+import org.havi.ui.HStaticText;
+import org.havi.ui.HTextButton;
+import org.havi.ui.HVisible;
+import org.havi.ui.event.HActionListener;
 
 public class HelloTVXlet implements Xlet {
 
